195502803c681bc8d958106b82827c9b53acee72
Duncan Murchison <duncancmurchison@gmail.com>
2023-01-01 13:28:08 -0500

    couple a functions for fun

--- tic_tac_toe/version_2_ai/ai_cpu_player.rb
+++ tic_tac_toe/version_2_ai/ai_cpu_player.rb
@@ -5,8 +5,28 @@ class ComputerPlayer
   attr_accessor :board
 
   def initialize
-    @board
+    @board = Board.new
   end
 
+  def get_move
+    puts "Computer's turn"
+    sleep(1)
+    @board.display_board
+    puts "Computer is thinking..."
+    sleep(1)
+    @board.available_spaces.sample
+  end
+
+  def make_move(move)
+    @board.update_board(move, "O")
+  end
+
+  def game_over?
+    @board.game_over?
+  end
+
+
 end
+
+# Adding a computer player to tic tac toe game in Ruby
 
